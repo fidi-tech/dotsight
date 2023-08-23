961e3defa19acc07e5711f86983bfcb035c3448e
aryabov <erazorrr@gmail.com>
2023-08-23 20:45:54 +0300

    fixed unit tests

--- src/pipelines/pipelines.controller.spec.ts
+++ src/pipelines/pipelines.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PipelinesController } from './pipelines.controller';
+import { ExecutePipelineService } from './services/execute-pipeline/execute-pipeline.service';
+import { PipelineService } from './services/pipeline/pipeline.service';
+import { MapperService } from '../mappers/services/mapper/mapper.service';
+import { DataSourceService } from '../data-sources/services/data-source/data-source.service';
+import { MixerService } from '../mixers/services/mixer/mixer.service';
 
 describe('PipelinesController', () => {
   let controller: PipelinesController;
@@ -7,6 +12,13 @@ describe('PipelinesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PipelinesController],
+      providers: [
+        ExecutePipelineService,
+        PipelineService,
+        MapperService,
+        DataSourceService,
+        MixerService,
+      ],
     }).compile();
 
     controller = module.get<PipelinesController>(PipelinesController);
--- src/pipelines/services/execute-pipeline/execute-pipeline.service.spec.ts
+++ src/pipelines/services/execute-pipeline/execute-pipeline.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExecutePipelineService } from './execute-pipeline.service';
+import { PipelineService } from '../pipeline/pipeline.service';
+import { MapperService } from '../../../mappers/services/mapper/mapper.service';
+import { DataSourceService } from '../../../data-sources/services/data-source/data-source.service';
+import { MixerService } from '../../../mixers/services/mixer/mixer.service';
 
 describe('ExecutePipelineService', () => {
   let service: ExecutePipelineService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExecutePipelineService],
+      providers: [
+        ExecutePipelineService,
+        PipelineService,
+        MapperService,
+        DataSourceService,
+        MixerService,
+      ],
     }).compile();
 
     service = module.get<ExecutePipelineService>(ExecutePipelineService);
