4a83e2d05f4c7a243a9650dea0482f7dbb067bbb
fidi-anton <102092268+fidi-anton@users.noreply.github.com>
2023-11-17 21:50:07 +0300

    rpc erc20 walletToken data source (#11)

--- src/data-sources/collection/index.ts
+++ src/data-sources/collection/index.ts
@@ -3,6 +3,7 @@ import { DebankWalletDatasource } from './debank/wallet.datasource';
 import { DebankWalletTokenDatasource } from './debank/wallet-token.datasource';
 import { DebankWalletTokenProtocolDatasource } from './debank/wallet-token.protocol.datasource';
 import { GiantSquidStatsWalletTokenDataSource } from './giant-squid/stats/wallet-token.datasource';
+import { Erc20Datasource } from './rpc/erc20.datasource';
 
 export const collection = {
   'dapp-radar-protocols': DappRadarProtocolDatasource,
@@ -10,6 +11,7 @@ export const collection = {
   'debank-wallet-tokens': DebankWalletTokenDatasource,
   'debank-protocol-tokens': DebankWalletTokenProtocolDatasource,
   'giant-squid-stats-wallet-token': GiantSquidStatsWalletTokenDataSource,
+  'erc20-wallet-tokens': Erc20Datasource,
 } as const;
 
 // checking if required static methods are implemented
---
+++ src/data-sources/collection/rpc/erc20.datasource.ts
@@ -0,0 +1,123 @@
+import { Contract, Web3 } from 'web3';
+import {
+  AbstractWalletTokenDataSource,
+  WalletTokenMeta,
+} from '../../abstract.wallet-token.data-source';
+import { EVM_ADDRESS, URL_REGEXP } from '../../../common/regexp';
+import { WalletToken } from '../../../entities/wallet-token.entity';
+import { abi } from './erc20.abi.json';
+
+type Config = {
+  rpcUrl: string;
+  contractAddress: string;
+};
+
+type Params = {
+  walletIds: string[];
+};
+
+export class Erc20Datasource extends AbstractWalletTokenDataSource<
+  Config,
+  Params
+> {
+  private contract: Contract<any>;
+
+  private isInited: boolean;
+  private tokenName: string;
+  private tokenSymbol: string;
+  private tokenDecimals: number;
+
+  constructor(config: Config) {
+    super(config);
+
+    const web3 = new Web3(config.rpcUrl);
+    this.contract = new web3.eth.Contract(abi, config.contractAddress);
+  }
+
+  public static getConfigSchema(): object {
+    return {
+      title: 'Config',
+      description: 'Erc20Datasource configuration',
+      type: 'object',
+      properties: {
+        rpcUrl: {
+          description: 'Url of the RPC endpoint',
+          type: 'string',
+          pattern: URL_REGEXP,
+        },
+        contractAddress: {
+          description: 'ERC20 contract address',
+          type: 'string',
+          pattern: EVM_ADDRESS,
+        },
+      },
+      required: ['rpcUrl', 'contractAddress'],
+    };
+  }
+
+  private async init() {
+    if (this.isInited) {
+      return;
+    }
+
+    [this.tokenName, this.tokenDecimals, this.tokenSymbol] = await Promise.all([
+      this.contract.methods.name().call().then(String),
+      this.contract.methods.decimals().call().then(Number),
+      this.contract.methods.symbol().call().then(String),
+    ]);
+
+    this.isInited = true;
+  }
+
+  private async getWalletBalance(walletId: string): Promise<number> {
+    const amount: number = await this.contract.methods
+      .balanceOf(
+        // @ts-expect-error bad web3 typings
+        walletId,
+      )
+      .call();
+    return Number(amount) / 10 ** this.tokenDecimals;
+  }
+
+  async getItems({
+    walletIds,
+  }: Params): Promise<{ items: WalletToken[]; meta: WalletTokenMeta }> {
+    await this.init();
+
+    const items = await Promise.all(
+      walletIds.map(async (walletId) => {
+        const amount = await this.getWalletBalance(walletId);
+        return {
+          id: `${walletId}-${this.config.contractAddress}`,
+          entity: 'walletToken' as const,
+          meta: {
+            id: this.config.contractAddress,
+            walletId,
+            symbol: this.tokenSymbol,
+            name: this.tokenName,
+          },
+          historicalMetrics: {
+            amount: [
+              {
+                timestamp: Math.floor(Date.now() / 1000),
+                value: amount,
+              },
+            ],
+          },
+          metrics: {
+            amount,
+          },
+        };
+      }),
+    );
+
+    return {
+      items,
+      meta: {
+        protocols: {},
+        chains: {},
+        units: {},
+      },
+    };
+  }
+}
