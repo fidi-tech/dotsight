274443cb0208903e54e1e66aba9f2a47325cae28
Anton <anton@fidi.tech>
2024-01-29 19:51:03 +0300

    added id collision check

--- src/categories/collection/index.ts
+++ src/categories/collection/index.ts
@@ -1,11 +1,21 @@
 import { NetworkCategory } from './network.category';
 
+class CategoriesIdCollisionError extends Error {
+  constructor(id: string) {
+    super(`Two categories with the same id ${id} encountered`);
+  }
+}
+
 export const categories = {
   [NetworkCategory.getId()]: NetworkCategory,
 } as const;
 
+const idCollisionMap = new Set<string>();
 for (const category of Object.values(categories)) {
-  category.getId();
+  const id = category.getId();
+  if (idCollisionMap.has(id)) {
+    throw new CategoriesIdCollisionError(id);
+  }
   category.getName();
   category.getIcon();
   category.validateSubcategory('smth');
