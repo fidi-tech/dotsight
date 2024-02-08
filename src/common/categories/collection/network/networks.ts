ddec946516ef1aa15d2e44f826432f92b558f191
Anton <anton@fidi.tech>
2024-02-08 19:21:16 +0300

    added networks

--- src/common/categories/collection/network/networks.ts
+++ src/common/categories/collection/network/networks.ts
@@ -1,3 +1,5 @@
 export const networks = [
   { id: 'ethereum', name: 'Ethereum', icon: null },
+  { id: 'avalanche', name: 'Avalanche', icon: null },
+  { id: 'arbitrum', name: 'Arbitrum', icon: null },
 ] as const;
