7dfe482055dfd9770a8686ccdbb2c53e0cdccf2d
Anton <anton@fidi.tech>
2024-02-25 14:41:56 +0300

    removed unused dep

--- src/data-sources/data-sources.module.ts
+++ src/data-sources/data-sources.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { CsvModule } from 'nest-csv-parser';
 import { DataSourceService } from './services/data-source/data-source.service';
 
 @Module({
-  imports: [CsvModule],
   providers: [DataSourceService],
   exports: [DataSourceService],
 })
