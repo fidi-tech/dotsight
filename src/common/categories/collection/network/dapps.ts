8f6e3059d3ba002785d2ed59f49fe59c5179798e
Anton <anton@fidi.tech>
2024-03-22 21:14:18 +0300

    added new dapps

--- src/common/categories/collection/network/dapps.ts
+++ src/common/categories/collection/network/dapps.ts
@@ -2,4 +2,7 @@ export const dapps = [
   { id: '46618', name: 'Jupiter Exchange', icon: null },
   { id: '33108', name: 'PancakeSwap V3', icon: null },
   { id: '39194', name: 'SyncSwap', icon: null },
+  { id: '13291', name: 'Beamswap', icon: null },
+  { id: '13814', name: 'StellaSwap', icon: null },
+  { id: '15173', name: 'ArthSwap', icon: null },
 ] as const;
