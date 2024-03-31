e0e908cd9e6034e9ea348cd0ecddeb1b757c633a
fidi-anton <102092268+fidi-anton@users.noreply.github.com>
2024-03-31 12:48:34 +0300

    new unit tests & bug fixes (#46)

--- src/widgets/services/widget/widget.service.spec.ts
+++ src/widgets/services/widget/widget.service.spec.ts
@@ -13,12 +13,16 @@ import { DataSourceService } from '../../../data-sources/services/data-source/da
 describe('WidgetService', () => {
   let service: WidgetService;
   let widgetAbilityService: WidgetAbilityService;
+  let categoriesService: CategoriesService;
   let repository: Repository<Widget>;
 
   beforeEach(async () => {
     repository = {
       find: jest.fn(),
       findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      delete: jest.fn(),
     } as any as Repository<Widget>;
 
     const module: TestingModule = await Test.createTestingModule({
@@ -44,6 +48,15 @@ describe('WidgetService', () => {
 
     widgetAbilityService =
       module.get<WidgetAbilityService>(WidgetAbilityService);
+    categoriesService = module.get<CategoriesService>(CategoriesService);
+  });
+
+  beforeEach(() => {
+    jest
+      .spyOn(widgetAbilityService, 'addAbilities')
+      .mockImplementation((userId, widget: any) => {
+        widget.ability = `${userId}-${widget.id}`;
+      });
   });
 
   describe('queryWidgets', () => {
@@ -53,11 +66,6 @@ describe('WidgetService', () => {
       const offset = 13;
       const query = 'anything';
 
-      jest
-        .spyOn(widgetAbilityService, 'addAbilities')
-        .mockImplementation((userId, widget: any) => {
-          widget.ability = `${userId}-${widget.id}`;
-        });
       const result = [{ id: 'w1' }, { id: 'w2' }] as any as Widget[];
       jest.spyOn(repository, 'find').mockResolvedValue(result);
 
@@ -86,11 +94,6 @@ describe('WidgetService', () => {
       const userId = '42';
       const widgetId = '66';
 
-      jest
-        .spyOn(widgetAbilityService, 'addAbilities')
-        .mockImplementation((userId, widget: any) => {
-          widget.ability = `${userId}-${widget.id}`;
-        });
       const result = { id: widgetId } as any as Widget;
       jest.spyOn(repository, 'findOne').mockResolvedValue(result);
 
@@ -104,6 +107,111 @@ describe('WidgetService', () => {
         where: { id: widgetId },
         relations: ['createdBy'],
       });
+    });
+  });
+
+  describe('create', () => {
+    it('should create widget in the repository & enrich it with abilities', async () => {
+      const userId = '42';
+      const category = 'cat';
+      const name = 'n1';
+
+      jest
+        .spyOn(repository, 'create')
+        .mockResolvedValue({ id: 'w1' } as any as never);
+      jest.spyOn(repository, 'save').mockResolvedValue(undefined);
+
+      await expect(service.create(userId, category, name)).resolves.toEqual({
+        id: 'w1',
+        ability: `${userId}-w1`,
+      });
+
+      expect(repository.create).toHaveBeenCalledTimes(1);
+      expect(repository.create).toHaveBeenCalledWith({
+        category,
+        name,
+        createdBy: { id: userId },
+      });
+      expect(repository.save).toHaveBeenCalledTimes(1);
+      expect(repository.save).toHaveBeenCalledWith(
+        expect.objectContaining({ id: 'w1' }),
+      );
+    });
+  });
+
+  describe('deleteWidget', () => {
+    it('should delete the widget from the repository', async () => {
+      const widgetId = '42';
+
+      jest.spyOn(repository, 'delete').mockResolvedValue(undefined);
+
+      await expect(service.deleteWidget(widgetId)).resolves.toEqual(undefined);
+
+      expect(repository.delete).toHaveBeenCalledTimes(1);
+      expect(repository.delete).toHaveBeenCalledWith({ id: widgetId });
+    });
+  });
+
+  describe('querySubcategories', () => {
+    it('should return an empty array if category is not selected', async () => {
+      const userId = '66';
+      const widgetId = '42';
+      jest.spyOn(repository, 'findOne').mockResolvedValue({
+        id: widgetId,
+        category: undefined,
+      } as any as Widget);
+
+      await expect(
+        service.querySubcategories(userId, widgetId),
+      ).resolves.toEqual([]);
+    });
+
+    it('should return selected subcategories & subcategories by the query', async () => {
+      const userId = '66';
+      const widgetId = '42';
+      const category = 'cat';
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue({
+        id: widgetId,
+        category,
+        subcategories: ['1', '3'],
+      } as any as Widget);
+      jest.spyOn(categoriesService, 'findSubcategories').mockResolvedValue([
+        { name: 'bc1', id: '1', icon: 'i1' },
+        { name: 'ac2', id: '2', icon: 'i2' },
+      ]);
+      jest
+        .spyOn(categoriesService, 'findSubcategoriesByIds')
+        .mockResolvedValue([
+          { name: 'bc1', id: '1', icon: 'i1' },
+          { name: 'cc3', id: '3', icon: 'i3' },
+        ]);
+
+      await expect(
+        service.querySubcategories(userId, widgetId),
+      ).resolves.toEqual([
+        {
+          icon: 'i2',
+          id: '2',
+          isAvailable: true,
+          isSelected: false,
+          name: 'ac2',
+        },
+        {
+          icon: 'i1',
+          id: '1',
+          isAvailable: true,
+          isSelected: true,
+          name: 'bc1',
+        },
+        {
+          icon: 'i3',
+          id: '3',
+          isAvailable: true,
+          isSelected: true,
+          name: 'cc3',
+        },
+      ]);
     });
   });
 });
